75e6a732b8e9d56287f8246957b74164d6f6b102
prescottlewis707 <prescottlewis707@gmail.com>
2020-05-03 15:13:43 +0100

    post requests

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.get('/courses', (req, res) => {
     res.status(200).json({ name: 'api testing' });
 });
 
 app.get('/course', (req, res) => {
     let name = req.query.name;
-    
     res.json({ id: '1', name: name });
 });
 
@@ -19,6 +21,16 @@ app.get('/course/:id', (req, res) => {
         name = 'mocha';
     };
     res.json({ id: id, name: name });
+});
+
+app.post('/course', (req, res) => {
+    let name = req.body.name;
+    res.json({ id: '2', name: name });
+});
+
+app.post('/course', (req, res) => {
+    let name = req.body.name;
+    res.json({ id: '2', name: name });
 });
 
 module.exports = app;
